70a1c4bf5d761fdc761877975618d1b0d2cbb6cc
Dillon Barker <dor.barker@gmail.com>
2016-04-21 22:18:16 -0600

    blast search

--- fngr-report.py
+++ fngr-report.py
@@ -2,6 +2,7 @@
 
 from Bio import SeqIO
 from Bio.Application import NcbiblastnCommandline as blastn
+from Bio.Blast import NCBIXML
 import json
 
 class Reporter(object):
@@ -15,25 +16,44 @@ class Reporter(object):
         self.report = {}
 
     def _load_genome(filepath):
-      
+
         with open(filepath, 'r') as f:
             g = {contig.id: contig.seq for contig in SeqIO.parse(f, 'fasta')}
         return g
 
     def _subseq(self, contig, start_stop):
-        
+
         start, stop = start_stop
         return self.genome[contig][start:stop]
-    
-    def _blast(self, query, top):
+
+    def _blast(self, possibly_foreign):
 
         if self.nt_path:
-            pass
-        else:
-            return None
 
-    def _create_json(self):
-        pass
+            hits = []
+
+            q = blastn(query = possibly_foreign, db = self.nt_path, outfmt = 5)
+
+            stdout, stderr = q()
+            result = NCBIXML.read(stdout)
+
+            for aln in result.alignments:
+                if len(hits) < top:
+                    hsp = next(aln.hsps) # top hit for each alignment
+                    hits.append(aln.title)
+                else:
+                    break
+
+            out = hits
+
+        return None and out
+
+    def _create_json(self, index):
+
+        start, stop = index
+        out = {'index': {'start': start, 'stop': stop},
+                pass
+                }
 
     def report(self):
         pass
