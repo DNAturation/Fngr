07c6e709be58f40e1c0eb72a3164ec7353fd505d
Dillon Barker <dor.barker@gmail.com>
2016-06-08 13:51:24 -0600

    Core functionality for decon.py

--- decon.py
+++ decon.py
@@ -2,6 +2,9 @@
 
 import argparse
 from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+import json
+import os
 
 def arguments():
 
@@ -24,9 +27,49 @@ def arguments():
 
     return parser.parse_args()
 
+def locate_contaminant_names(fngr, threshold):
+
+    with open(fngr) as f:
+        data = json.load(f)
+
+    return [contig for contig in data
+            if float(data[contig][0]['length']['ratio']) >= threshold]
+
+def separate_contigs(genome, contaminants):
+
+    good, bad = {}, {}
+
+    with open(genome) as g:
+        for rec in SeqIO.parse(g, 'fasta'):
+
+            if rec.id in contaminants:
+
+                bad[rec.id] = rec.seq
+                continue
+
+            good[rec.id] = rec.seq
+
+    return good, bad
+
+def write_output(contigs, name, ext, outpath):
+
+    if not os.access(os.path.join(outpath, name), os.F_OK):
+        os.mkdir(os.path.join(outpath, name))
+
+    with open(os.path.join(outpath, name, name + ext),'w') as o:
+        seqs = (SeqRecord(contigs[rec], rec, description='') for rec in contigs)
+        SeqIO.write(sorted(seqs, key=lambda x: x.id), o, 'fasta')
+
 def main():
 
     args = arguments()
+
+    name = os.path.splitext(os.path.basename(args.genome))[0]
+    contaminants = locate_contaminant_names(args.fngr, args.threshold)
+    good, bad = separate_contigs(args.genome, contaminants)
+
+    write_output(good, name, '.fasta', args.output)
+    write_output(bad, name, '.contamination', args.output)
 
 if __name__ == '__main__':
     main()
